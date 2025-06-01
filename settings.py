8aaea3cdcca63a69a14c24e30b61b8830ac94b03
Alvila25 <florentmandoyves43@gmail.com>
2025-06-01 14:10:10 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 # Use this for production:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+LANGUAGES = [
+    ('en', _('English')),
+    ('fr', _('French')),
+    ('ar', _('Arabic')),
+]
