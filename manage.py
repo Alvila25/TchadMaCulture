3a7c88deb2f78ccf09e855739a067eb0c00ebe95
Alvila25 <florentmandoyves43@gmail.com>
2025-06-13 00:17:28 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
+# Ensure the project root is in the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TchadMaCulture.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tchad_ma_culture.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +18,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
