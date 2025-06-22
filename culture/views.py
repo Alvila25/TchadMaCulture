f3ae63fce288115aa578795e4d21ffc81dbd361b
Alvila25 <florentmandoyves43@gmail.com>
2025-06-22 23:16:43 +0300

    Update views.py

--- culture/views.py
+++ culture/views.py
@@ -1,26 +1,5 @@
-from django.shortcuts import render, redirect
-from django.utils.translation import gettext as _
-from .models import Event, Artisan, BlogPost, Subscriber
+# culture/views.py
+from django.shortcuts import render
 
 def home(request):
-    events = Event.objects.all()
-    artisans = Artisan.objects.all()
-    blog_posts = BlogPost.objects.all().order_by('-created_at')[:1]  # Latest post
-    message = ''
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        if email:
-            Subscriber.objects.get_or_create(email=email)
-            message = _("Thank you for subscribing!")
-            return render(request, 'home.html', {
-                'events': events,
-                'artisans': artisans,
-                'blog_posts': blog_posts,
-                'message': message,
-            })
-    return render(request, 'home.html', {
-        'events': events,
-        'artisans': artisans,
-        'blog_posts': blog_posts,
-        'message': message,
-    })
+    return render(request, 'home.html')
