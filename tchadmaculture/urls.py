bb058488c4167fc5434853637b677a7ffb5c71f5
Alvila25 <florentmandoyves43@gmail.com>
2025-06-22 23:06:19 +0300

    Update urls.py

--- tchadmaculture/urls.py
+++ tchadmaculture/urls.py
@@ -1,16 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from culture.views import home
 
-urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),  # language switching views
-]
-
-urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('', include('culture.urls')),
+urlpatterns = i18n_patterns(
+    path('', home, name='home'),
+    path('i18n/', include('django.conf.urls.i18n')),
 )
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
