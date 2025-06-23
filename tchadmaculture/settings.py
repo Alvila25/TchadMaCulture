f17e90e178c11213d7426053b71b1c6108d1907a
Alvila25 <florentmandoyves43@gmail.com>
2025-06-23 13:53:54 +0300

    Update settings.py

--- tchadmaculture/settings.py
+++ tchadmaculture/settings.py
@@ -1,48 +1,24 @@
 import os
-from pathlib import Path
-from django.utils.translation import gettext_lazy as _
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-INSTALLED_APPS = [
-    'culture.apps.CultureConfig',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEBUG = True  # Set to False for production after testing
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        ...
+    },
 ]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-]
-
-LANGUAGE_CODE = 'en'  # Default site language
-
 LANGUAGES = [
-    ('en', _('English')),
-    ('fr', _('French')),
-    ('ar', _('Arabic')),
-    ('es',_('Spanish')), 
+    ('en', 'English'),
+    ('fr', 'French'),
+    ('ar', 'Arabic'),
+    ('es', 'Spanish'),
 ]
-
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR, 'locale'),
-]
-
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-TIME_ZONE = 'UTC'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticbuild')
